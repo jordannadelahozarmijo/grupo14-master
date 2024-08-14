ea92a4fc81f0923e3eb06a5ab257bbd3cf0d9992
AndresLh <andresllancapnh@gmail.com>
2024-08-14 11:45:23 -0400

    actualizacion

--- M4/M4S3/assets/js/main.js
+++ M4/M4S3/assets/js/main.js
@@ -16,3 +16,31 @@
     //utilizar el valor por defecto 1000 al seleccionar el input de propinas
         //Enviar alert de Propina enviada / Propina no enviada
 
+// Definir clase Pedido
+class Pedido {
+    constructor(total, propina = 1000) {
+        this.total = total;
+        this.propina = propina;
+    }
+
+    describir() {
+        return `Total: ${this.total}, Propina: ${this.propina}`;
+    }
+}
+
+// Definir clase Pizza que extiende de Pedido
+class Pizza extends Pedido {
+    // Definir constructor con parámetros adicionales y parámetros por defecto
+    constructor(total, propina, precio=15000,ingredientes) {
+        // Llamar al constructor de la clase padre con parámetros específicos
+        super(total, propina);
+        // Asignar valor a la propiedad precio
+        this.precio = precio;
+        this.ingredientes = ingredientes;
+    }
+
+    describir() {
+        return `Total: ${this.total}, Propina: ${this.propina}, Precio: ${this.precio}, Ingredientes ${this.ingredientes}.`;
+    }
+}
+
