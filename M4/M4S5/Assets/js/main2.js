b26de55731691416ecc91ffe676ebe7c0cae0610
AndresLh <andresllancapnh@gmail.com>
2024-08-19 12:58:02 -0400

    comentado

--- M4/M4S5/Assets/js/main2.js
+++ M4/M4S5/Assets/js/main2.js
@@ -1,16 +1,18 @@
-//
+//evento click con Jquery
 $(function () {
     $("h4").click(function () {
         $(this).fadeOut();
     });
 });
-
+//evento mouseenter y mouseleave
 $(function () {
+    //pasar el cursor por arriba
     $("h3").mouseenter(function () {
         $(this).animate({marginLeft: '50px'}, "slow", function () {
             $(this).css({ "color": "blue", "backgroundColor": "yellow"})
         })
     });
+    //sacar el cursor de arriba
     $("h3").mouseleave(function () {
         $(this).animate({marginRight: '50px',width: '10em'}, "fast", function () {
             $(this).css({ "color": "white", "backgroundColor": "black"})
