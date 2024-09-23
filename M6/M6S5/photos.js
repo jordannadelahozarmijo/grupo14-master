199c32f502f29a98236803c45643959900c85cb0
JordannaDelahoz <jddelahoz@uc.cl>
2024-09-23 11:07:42 -0300

    actualizar

--- M6/M6S5/photos.js
+++ M6/M6S5/photos.js
@@ -1,7 +1,7 @@
 
 
 async function getPhotos() {
-    const fetch = require('node-fetch');
+  const fetch = require('node-fetch');
   const response = await fetch('https://jsonplaceholder.typicode.com/photos');
   const data = await response.json();
   return data;
