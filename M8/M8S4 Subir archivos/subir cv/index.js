28a4fb9c3aeb4106e9fe546caf438441a89a2c52
JordannaDelahoz <jddelahoz@uc.cl>
2024-10-21 13:27:02 -0300

    upload

--- M8/M8S4 Subir archivos/subir cv/index.js
+++ M8/M8S4 Subir archivos/subir cv/index.js
@@ -1,43 +1,42 @@
 const express = require('express') // Cargando el modulo de Express.js
 const fileUpload = require('express-fileupload') // Cargando la librería de express-fileupload
 const PORT = 3000; // Este variable define el puerto del computador donde la API esta disponible
+const path = require ('path');
+const fs =  require('fs');
 
-// Definimos la variable que inicializa la libreria Express.js
+// Definimos la variable que inicializa la libreria Express.js y  Middleware
 const app = express();
-// Middleware
-app.use(fileUpload({createParentPath: true}));
+app.use(fileUpload());
+app.use(express.json()); // Para que la API pueda recibir datos en formato JSON
+app.use(express.static('public'));
 
 
+app.get('/', (req,res ) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.post('/upload', async (req, res) => {
 
     // Validando la no existencia de un archivo vacío
     if (!req.files || Object.keys(req.files).length === 0) {
-        res.send({
-        status: false,
+        res.status(400).send({
+        error: true,
+        codigo: 400,
         message: 'Archivo no subido al servidor',
-        error: "400"
         });
     } 
     else {
-        let fileRecived = req.files.fileName;
-        let extName = fileRecived.name;
-        uploadPath = './files/' + extName;
-        //Validando que el archivo sea solo texto plano .txt
-        if (fileRecived.mimetype === "text/plain") {
-
-            fileRecived.mv(uploadPath, err => {
-                    if (err) {
+        let archivo = req.files.fileName;
+        uploadPath = `./files/${archivo.name}`;
+        //Validando que el archivo no sea solo texto plano .txt
+        archivo.mv(uploadPath, err => {
+            if (err) {
                     return res.status(500).send({
                     message: err
                     });
-                    }
-                    return res.status(200).send({
-                    message: 'Archivo no válido, solo .txt'
-                    });
                 }
-            );
-        }
+            res.send ('Archivo subido con éxito');
+        });
     }
 });
 
