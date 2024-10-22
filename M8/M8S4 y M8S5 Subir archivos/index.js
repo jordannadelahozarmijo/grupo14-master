c4e49e192073fa6cf49eec72bb791d15ca6a8dcd
Andriu <andresllancapanh@gmail.com>
2024-10-22 13:08:04 -0300

    m8s5

--- M8/M8S4 y M8S5 Subir archivos/index.js
+++ M8/M8S4 y M8S5 Subir archivos/index.js
@@ -2,6 +2,7 @@ const express = require('express'); // Cargando el modulo de Express.js
 const fileUpload = require('express-fileupload'); // Cargando la librería de express-fileupload
 const path = require('path');
 const fs = require('fs');
+const baseUrl = "http://localhost:3000/files/";
 
 const PORT = 3000; // Definimos el puerto donde la API estará disponible
 
@@ -52,6 +53,62 @@ app.post('/upload', async (req, res) => {
         res.send('Archivo subido con éxito');
     });
 });
+
+//listar archivos
+app.get('/files', async (req, res) => {
+    // El método fs.readdir() se utiliza para leer de forma
+    // asíncrona el contenido de un directorio determinado.
+    fs.readdir(uploadDir, function(err, files) {
+        if (err) {
+        res.status(500).send({
+        message: "No se puede buscar archivos en el directorio!",
+        });
+        }
+        // Variable que contiene el listado de archivos en el servidor
+    
+        let listFiles = [];
+        files.forEach((file) => {
+            listFiles.push({
+                name: file,
+                url: baseUrl + file,
+            });
+        });
+
+        res.status(200).send(listFiles);
+    });
+});
+
+//descargar 
+app.get('/files/:name', async (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = "./files/";
+    // La función res.download() transfiere el archivo en la ruta
+    // como un "archivo adjunto". Por lo general, los navegadores
+    // le pedirán al usuario que descargue.
+    res.download(directoryPath + fileName, fileName, (err) => {
+        if (err) {
+            res.status(500).send({
+                message: "No se puede descargar el archivo. " + err,
+            });
+        }
+    });
+});
+// eliminar
+app.delete('/files/:name', async (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = "./files/";
+    try {
+        // fs.unlinkSync elimina un archivo y espera hasta que se terminela
+        // operación para seguir ejecutando el código, también se puede
+        // usar fs.unlink() que ejecuta dicha operación de forma asíncrona
+        fs.unlinkSync(directoryPath + fileName);
+        console.log('File removed')
+        res.status(200).send("Archivo Eliminado Satisfactoriamente");
+    } catch (err) {
+        console.error('ocurrió algo incorrecto al eliminar el archivo',err)
+    }
+});
+
 
 // Iniciar el servidor
 app.listen(PORT, () => {
